213ed5ac5283a0bc8822abd64571b6e0c4e1b7c3
mandydb04 <mariadeledibiase04@gmail.com>
2026-02-12 18:40:12 +0100

    Modifico

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
-import ExpenseForm from "./components/ExpenseForm";
-import ExpenseList from "./components/ExpenseList";
+import express from "express";
+import cors from "cors";
+import usersRoutes from "./routes/users.routes.js";
+import expensesRoutes from "./routes/expenses.routes.js";
 
-export default function App() {
-  return (
-    <>
-      <h1>Expense Split App</h1>
-      <ExpenseForm />
-      <ExpenseList />
-    </>
-  );
-}
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", usersRoutes);
+app.use("/expenses", expensesRoutes);
+
+export default app;
