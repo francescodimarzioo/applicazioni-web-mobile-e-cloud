fc5eca5785d89a085c70aea7c2e7075560fb82b5
mandydb04 <mariadeledibiase04@gmail.com>
2026-02-12 18:55:09 +0100

    ok

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,14 @@ import express from "express";
 import cors from "cors";
 import usersRoutes from "./routes/users.routes.js";
 import expensesRoutes from "./routes/expenses.routes.js";
+import mongoose from "mongoose";
 
+mongoose.connect("LA_TUA_MONGODB_URI_HERE", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("DB Connesso"))
+.catch(err => console.error("Errore DB:", err));
 const app = express();
 
 app.use(cors());
@@ -12,3 +19,4 @@ app.use("/users", usersRoutes);
 app.use("/expenses", expensesRoutes);
 
 export default app;
+
