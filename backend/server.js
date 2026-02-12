06ecb999219e8bca2ddab362b9196b9ec1675f6b
Fabiana Felicioni <fabian.felicioni@studenti.unicam.it>
2026-02-12 18:39:07 +0100

    modifica server

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const PORT = 3001;
 
 app.listen(PORT, () => {
-  console.log(console.log("Server avviato su http://localhost:" + PORT));
+  console.log("Server avviato su http://localhost:" + PORT);
 });
