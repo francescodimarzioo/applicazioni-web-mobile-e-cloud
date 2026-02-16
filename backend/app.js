c0b2fb0f7728d219d85743031ac78b3388c3139c
francescodimarzio <francescodimarzio2003@hotmail.com>
2026-02-16 15:40:51 +0100

    modifiche per la sicurezza del db

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import usersRoutes from "./routes/users.routes.js";
 import expensesRoutes from "./routes/expenses.routes.js";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://appusers:test1234@progettoapplicazioni.igsrw5g.mongodb.net/?appName=progettoapplicazioni")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Database connesso"))
   .catch(err => console.error("❌ Errore DB:", err));
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getExpenses() {
   const response = await fetch(API_URL + "/expenses");
