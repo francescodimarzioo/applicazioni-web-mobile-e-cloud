f08af61f86811c444822068de30b7e0ab49b3cbe
Francesco Di Marzio <francesco.dimarzio@studenti.unicam.it>
2026-03-02 15:33:54 +0100

    pulizia codice

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 
 import usersRoutes from "./routes/users.routes.js";
 import expensesRoutes from "./routes/expenses.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 
-/*mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Database connesso"))
-  .catch(err => console.error("❌ Errore DB:", err));*/
 
 const app = express();
 app.use(cors());
